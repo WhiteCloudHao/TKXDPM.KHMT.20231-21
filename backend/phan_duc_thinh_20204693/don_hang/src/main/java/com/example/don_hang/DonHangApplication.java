4089924ca234ec0e6666cb73fd6dbf1eea081119
thinhdev <phanthinhpdt@gmail.com>
2023-11-09 22:05:58 +0700

    second commit

---
+++ backend/phan_duc_thinh_20204693/don_hang/src/main/java/com/example/don_hang/DonHangApplication.java
@@ -0,0 +1,13 @@
+package com.example.don_hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonHangApplication.class, args);
+	}
+
+}
---
+++ backend/phan_duc_thinh_20204693/don_hang/src/test/java/com/example/don_hang/DonHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.don_hang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonHangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
